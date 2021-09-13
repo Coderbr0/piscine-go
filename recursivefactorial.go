474e5af7ac0b9376268ab4879b3e88306504b761
Coderbro <mo.abbaszadi@gmail.com>
2021-09-13 23:49:22 +0100

    Recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -7,9 +7,11 @@ func RecursiveFactorial(nb int) int {
 		return 1
 	}
 	result := 1
-
 	if nb > 1 {
-		return nb * RecursiveFactorial(nb-1)
+		result = nb * RecursiveFactorial(nb-1)
+		if result < 0 {
+			return 0
+		}
 	}
 	return result
 }
