aed0063e77b1ff93545d7e315ffd9bd36cf1925f
Coderbro <mo.abbaszadi@gmail.com>
2021-09-14 21:26:52 +0100

    Simulate Behaviour Join Function

---
+++ join.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Join(strs []string, sep string) string {
+	frazi := ""
+	first := true
+	for _, fraza := range strs {
+		if first {
+			frazi = fraza
+			first = false
+
+		} else {
+			frazi += sep + fraza
+		}
+	}
+	return frazi
+}
