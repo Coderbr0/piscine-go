e9fe45786a036f9a3544ab31befdeea314e849cf
Coderbro <mo.abbaszadi@gmail.com>
2021-09-21 21:50:52 +0100

    Return True If Slice of Integer Sorted

--- issorted.go
+++ issorted.go
@@ -1,30 +1,18 @@
 package piscine
 
-func Sign(a, b int) int {
-	if a == b {
-		return 0
-	} else if a > b {
-		return 1
-	} else {
-		return -1
-	}
-}
-
 func IsSorted(f func(a, b int) int, a []int) bool {
-	l := len(a) - 1
-	for i := range a {
-		if i < l-1 {
-			if f(a[i], a[i+1]) == 0 {
-				continue
-			} else if f(a[i], a[i+1]) > 0 {
-				if f(a[i+1], a[i+2]) < 0 {
+	if len(a) > 1 {
+		if f(a[0], a[1]) >= 0 {
+			for i := 0; i < len(a)-1; i++ {
+				if f(a[i], a[i+1]) < 0 {
 					return false
 				}
-				continue
-			} else if f(a[i], a[i+1]) < 0 {
-				if f(a[i+1], a[i+2]) < 0 {
-					return true
-				} else {
+			}
+		}
+
+		if f(a[0], a[1]) <= 0 {
+			for i := 0; i < len(a)-1; i++ {
+				if f(a[i], a[i+1]) > 0 {
 					return false
 				}
 			}
