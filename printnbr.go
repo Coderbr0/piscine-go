8ad321ec3c57856bc85cd7945ee0b7137b8e0a55
Coderbro <mo.abbaszadi@gmail.com>
2021-09-23 15:33:05 +0100

    Update

---
+++ printnbr.go
@@ -0,0 +1,34 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		if n/10 != 0 {
+			PrintNbr(n / -10)
+		}
+		d := '0'
+		for i := 0; i < -(n % 10); i += 1 {
+			d += 1
+		}
+		z01.PrintRune(d)
+	} else if n == 0 {
+		z01.PrintRune('0')
+	} else {
+		if n/10 != 0 {
+			PrintNbr(n / 10)
+		}
+		d := '0'
+		for i := 0; i < n%10; i += 1 {
+			d += 1
+		}
+		z01.PrintRune(d)
+	}
+}
+func main() {
+	PrintNbr(-123)
+	PrintNbr(0)
+	PrintNbr(123)
+	z01.PrintRune('\n')
+}
